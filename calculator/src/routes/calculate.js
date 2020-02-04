45fb377348109166a1c6fc624983912a95a9aaa6
unknown <milac0@hotmail.com>
2020-02-04 10:47:05 +0100

    first commit

---
+++ calculator/src/routes/calculate.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.route("/").get((req, res) => {
+  res.json({ msg: "Calculate!!" });
+});
+
+module.exports = router;
---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    (async () => {
+      const data = await fetch("http://localhost:3001/api/v1/calculate");
+      const result = await data.json();
+      setResult(result);
+    })();
+  }, []);
+
+  return <div className="title">{JSON.stringify(result)} !!</div>;
+};
+
+export default App;
