281a5582183599504e2d8e571dbbe7f3089e1071
unknown <milac0@hotmail.com>
2020-02-06 20:04:37 +0100

    connect mongo atlas

--- get-lorem/src/routes/lorem.ts
+++ get-lorem/src/routes/lorem.ts
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const Lorem = require('../models/lorem.model');
+import { Request, Response } from 'express';
+import { ILorem } from '../interface/ILorem';
 
-router.route("/").get((req, res) => {
-  res.json({ msg: "Lorem Ipsum!" });
+router.route("/").get((req: Request, res: Response) => {
+  Lorem.findOne({ name: "bacon" })
+    .then((lorem: ILorem) => res.status(200).json(lorem))
+    .catch((e: Error) => res.status(500).json({ msg: e }))
 });
 
 module.exports = router;
--- get-lorem/src/server.ts
+++ get-lorem/src/server.ts
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
+require('dotenv').config()
+
 const port = process.env.PORT || 3000;
 
 const loremRouter = require("./routes/lorem");
+
+mongoose.connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 app.use(cors());
 app.use(helmet());
