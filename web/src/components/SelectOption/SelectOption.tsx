d0f53e0eeff6cb8c3ff2268f09195a36646d1214
unknown <milac0@hotmail.com>
2020-02-07 19:31:57 +0100

    refactor state managment

--- web/src/components/SelectOption/SelectOption.tsx
+++ web/src/components/SelectOption/SelectOption.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./selectOption.scss";
 import Option from "../../enums/Option";
+import axios from "axios";
 
 interface Props {
-  handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  select: Option;
+  handleText: React.Dispatch<React.SetStateAction<string>>;
+  n: string;
 }
 
-const SelectOption: React.FC<Props> = ({ handleChange, select }) => {
+const SelectOption: React.FC<Props> = ({ handleText, n }) => {
+  const [select, setSelect] = useState(Option.bacon);
+
+  useEffect(() => {
+    (async () => {
+      const body = { name: select, n };
+      try {
+        const {
+          data: { string }
+        } = await axios.post("/lorem", body);
+        handleText(string);
+      } catch (e) {
+        handleText("Error downloading data. Try again!");
+      }
+    })();
+  }, [n, select]);
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelect(e.target.value as Option);
+  };
+
   return (
-    <div className={styles.text}>
-      <select onChange={handleChange} value={select}>
+    <div className={styles.option}>
+      <select onChange={handleSelect} value={select}>
         {Object.keys(Option).map((key: keyof typeof Option, i) => (
           <option key={i}>{key}</option>
         ))}
