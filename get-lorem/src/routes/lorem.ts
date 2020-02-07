bcb130abca61bfd1285368a60bc35290fecbd753
unknown <milac0@hotmail.com>
2020-02-07 11:12:04 +0100

    get lorem text route

--- get-lorem/src/routes/lorem.ts
+++ get-lorem/src/routes/lorem.ts
@@ -2,10 +2,21 @@ const router = require("express").Router();
 const Lorem = require('../models/lorem.model');
 import { Request, Response } from 'express';
 import { ILorem } from '../interface/ILorem';
+const getRandomNum = require('../../helpers/getRandomNum');
 
-router.route("/").get((req: Request, res: Response) => {
-  Lorem.findOne({ name: "bacon" })
-    .then((lorem: ILorem) => res.status(200).json(lorem))
+router.route("/").post((req: Request, res: Response) => {
+  const {name, n} = req.body
+  Lorem.findOne({ name })
+    .then((lorem: ILorem) => {
+      const words = lorem.words;
+      const max = words.length;
+      const arr = [];
+      for (let i=0; i<n; i++) {
+        arr.push(words[getRandomNum(max)])
+      }
+      const string = arr.join(' ');
+      return res.status(200).json({string});
+    })
     .catch((e: Error) => res.status(500).json({ msg: e }))
 });
 
