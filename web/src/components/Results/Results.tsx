e5e0a15c18f8780e340b376c2817df3d9b50df98
unknown <milac0@hotmail.com>
2020-02-08 18:09:52 +0100

    add similarity info

---
+++ web/src/components/Results/Results.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./results.scss";
+
+interface Props {
+  similarity: number;
+}
+
+const Results: React.FC<Props> = ({ similarity }) => {
+  const getPercentage = (num: number) => (num * 100).toFixed(2);
+
+  const getColor = (num: number) => {
+    let color = "";
+    if (num === 0) {
+      color = "black";
+    } else if (num <= 0.25) {
+      color = "grey";
+    } else if (num <= 0.5) {
+      color = "orange";
+    } else if (num <= 0.75) {
+      color = "blue";
+    } else {
+      color = "green";
+    }
+    return color;
+  };
+
+  return (
+    <div className={styles.results}>
+      <div style={{ backgroundColor: getColor(similarity) }}></div>
+      <h1>{getPercentage(similarity)}%</h1>
+    </div>
+  );
+};
+
+export default Results;
