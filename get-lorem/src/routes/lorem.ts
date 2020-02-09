f2abbb8e35c694a250ece8d111ea6bbaf5a4329b
unknown <milac0@hotmail.com>
2020-02-09 14:35:33 +0100

    set text streaming

--- get-lorem/src/routes/lorem.ts
+++ get-lorem/src/routes/lorem.ts
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const router = require("express").Router();
 const Lorem = require('../models/lorem.model');
 import { Request, Response } from 'express';
@@ -5,17 +6,22 @@ import { ILorem } from '../interface/ILorem';
 const getRandomNum = require('../../helpers/getRandomNum');
 
 router.route("/").post((req: Request, res: Response) => {
-  const {name, n} = req.body
+  const file = fs.createWriteStream('./text.txt');
+  const {name, n} = req.body;
+
   Lorem.findOne({ name })
     .then((lorem: ILorem) => {
       const words = lorem.words;
       const max = words.length;
-      const arr = [];
-      for (let i=0; i<n; i++) {
-        arr.push(words[getRandomNum(max)])
+
+      for (let i=0; i<n-1; i++) {
+          file.write(`${words[getRandomNum(max)]} `);
       }
-      const string = arr.join(' ');
-      return res.status(200).json({string});
+      file.write(words[getRandomNum(max)]);
+
+      const readStream = fs.createReadStream('./text.txt')
+      res.status(200);
+      readStream.pipe(res);
     })
     .catch((e: Error) => res.status(500).json({ msg: e }))
 });
